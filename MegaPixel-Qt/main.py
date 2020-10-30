e049f78c08173467bea6921a6ad74ad3030876f2
Alkl58 <34812414+Alkl58@users.noreply.github.com>
2020-10-30 09:37:28 +0100

    Added jpegxl

--- MegaPixel-Qt/main.py
+++ MegaPixel-Qt/main.py
@@ -17,6 +17,8 @@ class megapixel(QtWidgets.QMainWindow):
     imageOutput = ""
     avifParams = ""
     webpParams = ""
+    cjxlParams = ""
+    djxlParams = ""
 
     def __init__(self):
 
@@ -32,20 +34,74 @@ class megapixel(QtWidgets.QMainWindow):
         self.pushButtonClearQueue.clicked.connect(self.ClearQueue)
         self.comboBoxEncoders.currentIndexChanged.connect(self.ToggleUiElems)
         self.checkBoxCustomSettings.stateChanged.connect(self.ToggleCustomSettings)
+
         # Avif
         self.checkBoxAvifLossless.stateChanged.connect(self.ToggleAvifLossless)
+
         # Webp
         self.checkBoxWebpQuality.stateChanged.connect(self.ToggleWebpQuality)
         self.checkBoxWebpSize.stateChanged.connect(self.ToggleWebpSize)
         self.checkBoxWebpPSNR.stateChanged.connect(self.ToggleWebpPSNR)
         self.checkBoxWebpLossless.stateChanged.connect(self.ToggleWebpLossless)
+
+        # Jpegxl
+        self.checkBoxJpegXlQ.stateChanged.connect(self.ToggleJpegXlQ)
+        self.checkBoxJpegXlSize.stateChanged.connect(self.ToggleJpegXlSize)
+        self.checkBoxJpegXlEncode.stateChanged.connect(self.ToggleJpegXlEncode)
+        self.checkBoxJpegXlDecode.stateChanged.connect(self.ToggleJpegXlDecode)
+        self.comboBoxJpegXlDecodeFormat.currentIndexChanged.connect(self.ToggleJpegXlDecodeSettings)
+
+        # UI Toggle
+        self.checkBoxJpegXlDecodesjpeg.hide()
+        self.labelJpegXlDecodeQuality.hide()
+        self.spinBoxJpegXlDecodeQ.hide()
         self.groupBoxAvif.show()
         self.groupBoxWebp.hide()
+        self.groupBoxJpegXl.hide()
         self.show()  # Show the GUI
 
+    def ToggleJpegXlDecodeSettings(self):
+        if self.comboBoxJpegXlDecodeFormat.currentIndex() == 1:
+            self.checkBoxJpegXlDecodesjpeg.show()
+            self.labelJpegXlDecodeQuality.show()
+            self.spinBoxJpegXlDecodeQ.show()
+        else:
+            self.checkBoxJpegXlDecodesjpeg.hide()
+            self.labelJpegXlDecodeQuality.hide()
+            self.spinBoxJpegXlDecodeQ.hide()
 
+    def ToggleJpegXlEncode(self):
+        if self.checkBoxJpegXlEncode.isChecked() is True:
+            self.checkBoxJpegXlDecode.setChecked(False)
+            self.checkBoxJpegXlQ.setEnabled(True)
+            self.spinBoxJpegXlQ.setEnabled(True)
+            self.checkBoxJpegXlSize.setEnabled(True)
+            self.spinBoxJpegXlSize.setEnabled(True)
+            self.labelJpegXlSpeed.setEnabled(True)
+            self.comboBoxJpegXlSpeed.setEnabled(True)
+            self.labelJpegXlDecodeFormat.setEnabled(False)
+            self.comboBoxJpegXlDecodeFormat.setEnabled(False)
+            self.checkBoxJpegXlDecodesjpeg.setEnabled(False)
+            self.labelJpegXlDecodeQuality.setEnabled(False)
+            self.spinBoxJpegXlDecodeQ.setEnabled(False)
+
+    def ToggleJpegXlDecode(self):
+        if self.checkBoxJpegXlDecode.isChecked() is True:
+            self.checkBoxJpegXlEncode.setChecked(False)
+            self.checkBoxJpegXlQ.setEnabled(False)
+            self.spinBoxJpegXlQ.setEnabled(False)
+            self.checkBoxJpegXlSize.setEnabled(False)
+            self.spinBoxJpegXlSize.setEnabled(False)
+            self.labelJpegXlSpeed.setEnabled(False)
+            self.comboBoxJpegXlSpeed.setEnabled(False)
+            self.labelJpegXlDecodeFormat.setEnabled(True)
+            self.comboBoxJpegXlDecodeFormat.setEnabled(True)
+            self.checkBoxJpegXlDecodesjpeg.setEnabled(True)
+            self.labelJpegXlDecodeQuality.setEnabled(True)
+            self.spinBoxJpegXlDecodeQ.setEnabled(True)
+
+    # This function disables visually Min/Max Q when lossless is toggled
     def ToggleAvifLossless(self):
-        # This function disables visually Min/Max Q when lossless is toggled
         if self.checkBoxAvifLossless.isChecked() is True:
             self.spinBoxAvifMinQ.setEnabled(False)
             self.spinBoxAvifMaxQ.setEnabled(False)
@@ -57,8 +113,8 @@ class megapixel(QtWidgets.QMainWindow):
             self.labelAvifMinQ.setEnabled(True)
             self.labelAvifMaxQ.setEnabled(True)
 
+    # This function disables visually everything when custom settings is toggled
     def ToggleCustomSettings(self):
-        # This function disables visually everything when custom settings is toggled
         if self.checkBoxCustomSettings.isChecked() is True:
             self.textEditCustomSettings.setEnabled(True)
             if self.comboBoxEncoders.currentIndex() == 0:
@@ -69,6 +125,14 @@ class megapixel(QtWidgets.QMainWindow):
                 self.SetWebpParams(True)
                 self.textEditCustomSettings.setText(self.webpParams)
                 self.groupBoxWebp.setEnabled(False)
+            elif self.comboBoxEncoders.currentIndex() == 2:
+                if self.checkBoxJpegXlEncode.isChecked() is True:
+                    self.SetJpegXlParams(True)
+                    self.textEditCustomSettings.setText(self.cjxlParams)
+                else:
+                    self.SetJpegXlDecodeParams(True)
+                    self.textEditCustomSettings.setText(self.djxlParams)
+                self.groupBoxJpegXl.setEnabled(False)
         else:
             self.textEditCustomSettings.setEnabled(False)
             if self.comboBoxEncoders.currentIndex() == 0:
@@ -80,18 +144,26 @@ class megapixel(QtWidgets.QMainWindow):
                 self.groupBoxAvif.setEnabled(True)
             elif self.comboBoxEncoders.currentIndex() == 1:
                 self.groupBoxWebp.setEnabled(True)
+            elif self.comboBoxEncoders.currentIndex() == 2:
+                self.groupBoxJpegXl.setEnabled(True)
 
 
     def ToggleUiElems(self):
         if self.comboBoxEncoders.currentIndex() == 0:
-            self.groupBoxWebp.hide()
             self.groupBoxAvif.show()
+            self.groupBoxWebp.hide()
+            self.groupBoxJpegXl.hide()
         elif self.comboBoxEncoders.currentIndex() == 1:
             self.groupBoxAvif.hide()
             self.groupBoxWebp.show()
+            self.groupBoxJpegXl.hide()
+        elif self.comboBoxEncoders.currentIndex() == 2:
+            self.groupBoxAvif.hide()
+            self.groupBoxWebp.hide()
+            self.groupBoxJpegXl.show()
 
+    # Toggles Target Size, PSNR and Lossless visually off
     def ToggleWebpQuality(self):
-        # Toggles Target Size, PSNR and Lossless visually off
         if self.checkBoxWebpQuality.isChecked() is True:
             self.checkBoxWebpSize.setChecked(False)
             self.checkBoxWebpPSNR.setChecked(False)
@@ -101,8 +173,8 @@ class megapixel(QtWidgets.QMainWindow):
             self.spinBoxWebpQ.setEnabled(True)
             self.comboBoxWebpLossless.setEnabled(False)
 
-    def ToggleWebpSize(self):
-        # Toggles Quality, PSNR and Lossless visually off
+    # Toggles Quality, PSNR and Lossless visually off
+    def ToggleWebpSize(self):        
         if self.checkBoxWebpSize.isChecked() is True:
             self.checkBoxWebpQuality.setChecked(False)
             self.checkBoxWebpPSNR.setChecked(False)
@@ -112,8 +184,8 @@ class megapixel(QtWidgets.QMainWindow):
             self.spinBoxWebpQ.setEnabled(False)
             self.comboBoxWebpLossless.setEnabled(False)
 
-    def ToggleWebpPSNR(self):
-        # Toggles Quality, Target Size and Lossless visually off
+    # Toggles Quality, Target Size and Lossless visually off
+    def ToggleWebpPSNR(self):        
         if self.checkBoxWebpPSNR.isChecked() is True:
             self.checkBoxWebpQuality.setChecked(False)
             self.checkBoxWebpSize.setChecked(False)
@@ -123,8 +195,8 @@ class megapixel(QtWidgets.QMainWindow):
             self.spinBoxWebpQ.setEnabled(False)
             self.comboBoxWebpLossless.setEnabled(False)
 
+    # Toggles Quality, Target Size and PSNR visually off
     def ToggleWebpLossless(self):
-        # Toggles Quality, Target Size and PSNR visually off
         if self.checkBoxWebpLossless.isChecked() is True:
             self.checkBoxWebpQuality.setChecked(False)
             self.checkBoxWebpSize.setChecked(False)
@@ -133,6 +205,20 @@ class megapixel(QtWidgets.QMainWindow):
             self.spinBoxWebpSize.setEnabled(False)
             self.spinBoxWebpQ.setEnabled(False)
             self.comboBoxWebpLossless.setEnabled(True)
+
+    # Visually Toggles JpegXl Target Size / Quality
+    def ToggleJpegXlQ(self):
+        if self.checkBoxJpegXlQ.isChecked() is True:
+            self.checkBoxJpegXlSize.setChecked(False)
+            self.spinBoxJpegXlSize.setEnabled(False)
+            self.spinBoxJpegXlQ.setEnabled(True)
+
+    # Visually Toggles JpegXl Target Size / Quality
+    def ToggleJpegXlSize(self):
+        if self.checkBoxJpegXlSize.isChecked() is True:
+            self.checkBoxJpegXlQ.setChecked(False)
+            self.spinBoxJpegXlQ.setEnabled(False)
+            self.spinBoxJpegXlSize.setEnabled(True)
 
     def OpenImageSource(self):
         if self.checkBoxBatchAdd.isChecked() is True:
@@ -156,22 +242,23 @@ class megapixel(QtWidgets.QMainWindow):
         msgBox = QMessageBox()
         msgBox.setIcon(QMessageBox.Information)
         msgBox.setText("Encoding finished.")
-        msgBox.setWindowTitle("Encoding finished.")
+        msgBox.setWindowTitle("MegaPixel")
         msgBox.setStandardButtons(QMessageBox.Ok)
         msgBox.exec()
 
     def StartEncoding(self):
         self.SetAvifParams(False)
         self.SetWebpParams(False)
+        self.SetJpegXlParams(False)
         asyncio.run(self.Encode())
 
+    # Sets the params for avif encoding
     def SetAvifParams(self, custom):
-        # Sets the params for avif encoding
-        if self.checkBoxCustomSettings.isChecked is False or custom is True:
+        if self.checkBoxCustomSettings.isChecked() is False or custom is True:
             self.avifParams = " --depth " + self.comboBoxAvifDepth.currentText()
             self.avifParams += " --yuv " + self.comboBoxAvifColorFormat.currentText()
             self.avifParams += " --range " + self.comboBoxAvifRange.currentText()
-            if self.checkBoxAvifLossless.isChecked is False:
+            if self.checkBoxAvifLossless.isChecked() is False:
                 self.avifParams += " --min " + str(self.spinBoxAvifMinQ.value()) + " --max " + str(self.spinBoxAvifMaxQ.value())
             else:
                 self.avifParams += " --lossless"
@@ -181,7 +268,7 @@ class megapixel(QtWidgets.QMainWindow):
             self.avifParams = self.textEditCustomSettings.toPlainText()
 
     def SetWebpParams(self, custom):
-        if self.checkBoxCustomSettings.isChecked is False or custom is True:
+        if self.checkBoxCustomSettings.isChecked() is False or custom is True:
             self.webpParams = " -preset " + self.comboBoxWebpPreset.currentText()
             if self.checkBoxAvifLossless.isChecked() is False:
                 if self.checkBoxWebpPSNR.isChecked() is True:
@@ -200,6 +287,22 @@ class megapixel(QtWidgets.QMainWindow):
             else:
                 self.webpParams += " -z " + str(self.comboBoxWebpLossless.currentIndex())
 
+    def SetJpegXlParams(self, custom):
+        if self.checkBoxCustomSettings.isChecked() is False or custom is True:
+            self.cjxlParams = " --speed=" + str(self.comboBoxJpegXlSpeed.currentIndex() + 3)
+            if self.checkBoxJpegXlSize.isChecked() is True:
+                self.cjxlParams += " --target_size=" + str(self.spinBoxJpegXlSize.value() * 1000)
+            elif self.checkBoxJpegXlQ.isChecked() is True:
+                self.cjxlParams += " --quality=" + str(self.spinBoxJpegXlQ.value())
+
+    def SetJpegXlDecodeParams(self, custom):
+        if self.checkBoxCustomSettings.isChecked() is False or custom is True:
+            self.djxlParams = ""
+            if self.comboBoxJpegXlDecodeFormat.currentIndex() == 1:
+                self.djxlParams += " --jpeg_quality=" + str(self.spinBoxJpegXlDecodeQ.value())
+                if self.checkBoxJpegXlDecodesjpeg.isChecked() is True:
+                    self.djxlParams += " --use_sjpeg"
+
 
     async def Encode(self):
         commands = [ ]
@@ -209,11 +312,16 @@ class megapixel(QtWidgets.QMainWindow):
             if self.comboBoxEncoders.currentIndex() == 0:
                 avifCMD = "avifenc " + self.avifParams + " \"" + imageInput + "\" " + " \"" + imgOutput + ".avif\""
                 commands.append(avifCMD)
-                print(avifCMD)
             elif self.comboBoxEncoders.currentIndex() == 1:
                 webpCMD = "cwebp " + self.webpParams + " \"" + imageInput + "\" " + " -o \"" + imgOutput + ".webp\""
                 commands.append(webpCMD)
-                print(webpCMD)
+            elif self.comboBoxEncoders.currentIndex() == 2:
+                if self.checkBoxJpegXlEncode.isChecked() is True:
+                    cjxlCMD = "cjxl \"" + imageInput + "\"  \"" + imgOutput + ".jpg\" " + self.cjxlParams
+                    commands.append(cjxlCMD)
+                else:
+                    djxlCMD = "djxl \"" + imageInput + "\"  \"" + imgOutput + "." + self.comboBoxJpegXlDecodeFormat.currentText() + "\" " + self.djxlParams
+                    commands.append(djxlCMD)
 
         self.progressBar.setMaximum(len(commands))  # Sets the Max Value of Progressbar
         pool = Pool(self.spinBoxParallelWorkers.value())  # Sets the amount of workers
